1713110dd4888ba538cc254a8e99619c7fc15774
got <gotmadagascar@gmail.com>
2018-03-18 19:20:20 +0700

    Learned all forms of data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,7 +12,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus = 'No server was created'
+  serverCreationStatus = 'No server was created';
+  serverName = 'TestServer';
 
   constructor() {
     setTimeout(() => {
@@ -24,7 +25,11 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
